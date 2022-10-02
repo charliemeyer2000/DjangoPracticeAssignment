a49bbfe330791274165b3773297a6e770f62f1c0
Charlie Meyer <charlie@Charlies-MacBook-Pro-2.local>
2022-10-02 11:56:16 -0400

    changing settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d^hdcp+tj!d2%on89ktrfi0uyh*hg%vm^2=+*e7p4o_sipu#md
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['test-website-for-onboarding.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','test-website-for-onboarding.herokuapp.com']
 
 
 # Application definition
